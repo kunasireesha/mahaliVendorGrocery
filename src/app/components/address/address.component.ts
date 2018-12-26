e069ae1faa441e1a06f8f5ad613b5d7da50d1dee
kunasireesha <kunasireesha5@gmail.com>
2018-12-26 12:16:42 +0530

    changes

--- src/app/components/address/address.component.ts
+++ src/app/components/address/address.component.ts
@@ -210,7 +210,6 @@ export class AddressComponent implements OnInit {
             this.account_holder_name_errors = false;
             this.account_number_errors = true;
             return;
-            return;
         } else if (this.bankForm.value.bank_name === '') {
             this.account_holder_name_errors = false;
             this.account_number_errors = false;
@@ -269,10 +268,10 @@ export class AddressComponent implements OnInit {
 
 
 
-    showBusinessDetails :boolean =  true;
-    showStationaryData :boolean= false;
-    showBankDetails:boolean =  false;
-    showAdd:boolean= false;
+    showBusinessDetails: boolean = true;
+    showStationaryData: boolean = false;
+    showBankDetails: boolean = false;
+    showAdd: boolean = false;
 
     showBusiness() {
         this.showBusinessDetails = true;
